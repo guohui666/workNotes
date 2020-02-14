70b2178657da609b104c7f5e4535ae6dac3c0dcf
guohui <184142115@qq.com>
2020-02-14 18:40:00 +0800

    is指定组件避免标签bug

---
+++ vue/is.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="is">
+<!--        html规范-> tbody里只能有tr 否则会渲染错误 -->
+        <table>
+            <tbody>
+                <tr>
+                    <td></td>
+                </tr>
+            </tbody>
+        </table>
+<!--        使用is指定组件，避免和html规范冲突 其他配对的标签最好也指定，比如ul li -->
+        <table>
+            <tbody>
+                <tr is="row">
+                    <td></td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</template>
+
+<script>
+    export default {
+        components:{
+          row //假设有row子组件
+        },
+        props: {}
+    }
+</script>
